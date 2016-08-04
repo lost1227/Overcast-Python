a92b3020250e8eb1286bed9852f46ca01dc0e212
lost1227 <jordanpowers1227@gmail.com>
2016-08-04 16:04:57 -0700

    Updated to python 3

--- overcastLogin.py
+++ overcastLogin.py
@@ -2,13 +2,13 @@ def login():
 	import requests
 	import sys
 	s = requests.Session()
-	login = {'email': raw_input("Enter your email> "), 'password': raw_input("Enter your password> ")}
+	login = {'email': input("Enter your email> "), 'password': input("Enter your password> ")}
 	try:
 		r = s.post("https://overcast.fm/login", data=login)
 	except requests.exceptions.ConnectionError:
 		print("You are not connected to the Internet!")
 		exit()
 	print("Your UUID is : %s" % s.cookies["o"])
-	f = open("%s\UUID.txt" % sys.path[0],"w")
+	f = open("%s\\UUID.txt" % sys.path[0],"w")
 	f.write(s.cookies['o'])
 	f.close()
--- overcastParser.py
+++ overcastParser.py
@@ -14,29 +14,33 @@ def updateServers(player, time, version):
 # Open the internet session with the required cookies
 session = requests.session()
 # Login if not logged in
-if not os.path.isfile("%s\UUID.txt" % sys.path[0]):
+if not os.path.isfile("%s\\UUID.txt" % sys.path[0]):
 	print("No cookie detected, logging in!")
 	overcastLogin.login()
 
-uuid = open("%s\UUID.txt" % sys.path[0],"r")
+uuid = open("%s\\UUID.txt" % sys.path[0],"r")
 session.cookies.set("o",uuid.read())
 uuid.close()
 
 
 # An sqlite database will be used to keep track of the downloaded podcasts
 # A table in the database will track the podcast, title, file location, data-item-id, and play location of each podcast downloaded
-conn = sqlite3.connect("%s\overcast.db" % sys.path[0])
+conn = sqlite3.connect("%s\\overcast.db" % sys.path[0])
 c = conn.cursor()
 c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='podcasts';")
 if (c.fetchall() == []):
 	print("Creating new table!")
 	c.execute("CREATE TABLE podcasts (dataItemId INTEGER(15), time INTEGER, podcast VARCHAR(255), title VARCHAR(255), location VARCHAR(255));")
 
-res = raw_input("Download New?> ")
 while True:
+	res = input("Download New?> ")
 	if (res == "y"):
 		# Download the Overcast Homepage
-		mainPage = session.get("https://overcast.fm/")
+		try:
+			mainPage = session.get("https://overcast.fm/")
+		except requests.exceptions.ConnectionError:
+			print("Connection Error!")
+			break
 		# Parse the downloaded html
 		parsedOvercast = BeautifulSoup(mainPage.text,"html.parser")
 		allCasts = parsedOvercast.find_all("a", class_="episodecell")
@@ -49,7 +53,7 @@ while True:
 		while True:
 			try:
 				# Select a podcast, catching any out-of-range entries
-				selectedCast = (int(raw_input("Which podcast?> ")) - 1)
+				selectedCast = (int(input("Which podcast?> ")) - 1)
 				if ((selectedCast > podcastNum) or (selectedCast < 0)):
 					raise ValueError
 				break
@@ -62,22 +66,30 @@ while True:
 		# Download the podcast page
 		podcastHREF = "https://overcast.fm%s" % selCast.get('href')
 		print("This podcast is at %s" % podcastHREF)
-		podcastPage = session.get(podcastHREF)
+		try:
+			podcastPage = session.get(podcastHREF)
+		except requests.exceptions.ConnectionError:
+			print("Connection Error!")
+			break
 		# Parse the downloaded html
 		parsedPodcast = BeautifulSoup(podcastPage.text,"html.parser")
 		audioPlayer = parsedPodcast.find(id="audioplayer")
 		# Set the update number to the one provided by the server
 		# This must be set by the return value of the updateServers function every time it is called
 		serverUpdate = audioPlayer.get('data-sync-version')
-		if not os.path.exists("%s\podcasts\\%s\\" % (sys.path[0], selCastShow)):
+		if not os.path.exists("%s\\podcasts\\%s\\" % (sys.path[0], selCastShow)):
 			os.makedirs("%s\podcasts\\%s\\" % (sys.path[0], selCastShow))
-		with open ("%s\podcasts\\%s\\%s.mp3" % (sys.path[0], selCastShow, audioPlayer.get("data-item-id")), "wb") as pFile:
-			pResponse = session.get(audioPlayer.find("source").get("src"))
+		with open ("%s\\podcasts\\%s\\%s.mp3" % (sys.path[0], selCastShow, audioPlayer.get("data-item-id")), "wb") as pFile:
+			try:
+				pResponse = session.get(audioPlayer.find("source").get("src"), stream=True)
+			except requests.exceptions.ConnectionError:
+				print("Connection Error!")
+				break
 			for chunk in pResponse.iter_content(1024):
 				pFile.write(chunk)
 		
 		# Test code to see if setting the time works as expected
-		serverUpdate = updateServers(audioPlayer, raw_input("New Time?> "), serverUpdate)
+		serverUpdate = updateServers(audioPlayer, input("New Time?> "), serverUpdate)
 		print(serverUpdate)
 		
 		break
